6baa92d923ec9139bfb55bf22304e785876f93a3
Joseph Best <joejoebest@hotmail.co.uk>
2023-11-10 11:24:34 +0000

    Adds file with if statement

---
+++ milestone_2.py
@@ -0,0 +1,17 @@
+import random
+
+word_list = ['kiwi', 'strawberry', 'orange', 'lemon', 'apple']
+#print(word_list)
+
+word = random.choice(word_list)
+#print(word)
+
+guess = raw_input('Enter a single letter... ')
+
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess')
+else:
+    print('Oops, thats not a valid input!')
+
+
+  
